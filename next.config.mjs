a01f02dc6ff7c2d05d613b59637ed951216df8b5
Tim Lanoue <timlanoue@Tims-MacBook-Pro.local>
2025-11-07 17:58:15 -0800

    fix: ignore sanity studio during prerender build

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    appDir: true,
+  },
   output: 'standalone',
+
   async rewrites() {
     return [
       {
@@ -15,6 +16,21 @@ const nextConfig = {
         destination: '/studio/index.html',
       },
     ]
+  },
+
+  // 👇 this line prevents Studio from being statically built
+  generateBuildId: async () => {
+    return 'build-' + Date.now().toString()
+  },
+  // 👇 this ensures Studio is ignored during pre-render
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push({
+        '@sanity/vision': 'commonjs @sanity/vision',
+      })
+    }
+    return config
   },
 }
 
